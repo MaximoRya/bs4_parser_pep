616aa61cccb789c5aedd7629f7ba6dd939f3d838
Максим Рябов <ryabov.mv@yandex.ru>
2023-05-30 14:19:57 -0400

    rewiew_30.05

--- src/outputs.py
+++ src/outputs.py
@@ -9,9 +9,9 @@ from constants import BASE_DIR, DATETIME_FORMAT
 
 def control_output(pre_results, cli_args):
     titles = {
-        'p':('Cтатус', 'Количество'),
-        'l':('Ссылка на документацию', 'Версия', 'Статус'),
-        'w':('Ссылка на статью', 'Заголовок', 'Редактор, Автор')
+        'p': ('Cтатус', 'Количество'),
+        'l': ('Ссылка на документацию', 'Версия', 'Статус'),
+        'w': ('Ссылка на статью', 'Заголовок', 'Редактор, Автор')
     }
     results = [titles[str(cli_args).split(',')[0][16]]]
     results = results + pre_results
@@ -39,15 +39,15 @@ def pretty_output(results):
 
 
 def file_output(results, cli_args):
-    #тесты требуют наличие вычисления пути от базового католога :(
-    results_dir = BASE_DIR / 'results' 
-    results_dir.mkdir(exist_ok=True) 
-    parser_mode = cli_args.mode 
-    now = dt.datetime.now() 
-    now_formatted = now.strftime(DATETIME_FORMAT) 
-    file_name = f'{parser_mode}_{now_formatted}.csv' 
-    file_path = results_dir / file_name 
-    with open(file_path, 'w', encoding='utf-8') as f: 
-        writer = csv.writer(f, dialect='unix') 
-        writer.writerows(results) 
+    # тесты требуют наличие вычисления пути от базового католога :(
+    results_dir = BASE_DIR / 'results'
+    results_dir.mkdir(exist_ok=True)
+    parser_mode = cli_args.mode
+    now = dt.datetime.now()
+    now_formatted = now.strftime(DATETIME_FORMAT)
+    file_name = f'{parser_mode}_{now_formatted}.csv'
+    file_path = results_dir / file_name
+    with open(file_path, 'w', encoding='utf-8') as f:
+        writer = csv.writer(f, dialect='unix')
+        writer.writerows(results)
     logging.info(f'Файл с результатами был сохранён: {file_path}')
